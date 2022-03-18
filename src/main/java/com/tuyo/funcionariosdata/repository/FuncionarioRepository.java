4cf5c0fb62fc5a4dc3122bebcefdfadbe96e1381
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-18 17:47:11 -0300

    Delete src directory

--- src/main/java/com/tuyo/funcionariosdata/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tuyo.funcionariosdata.repository;
-
-import com.tuyo.funcionariosdata.entities.Funcionario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
-}
