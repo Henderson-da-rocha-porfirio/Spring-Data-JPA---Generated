763c93610a828f4cca825fcd8c157fab8f1ff063
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-17 21:42:41 -0300

    # Commit - 1 - master

---
+++ src/main/java/com/tuyo/funcionariosdata/FuncionariosdataApplication.java
@@ -0,0 +1,13 @@
+package com.tuyo.funcionariosdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionariosdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuyo/funcionariosdata/FuncionariosdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyo.funcionariosdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
