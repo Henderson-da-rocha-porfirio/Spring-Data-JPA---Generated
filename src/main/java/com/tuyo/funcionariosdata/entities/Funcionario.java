f3c65499e0832081669b2cf87c02488cee14f717
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-18 21:44:04 -0300

    # commit 5 - Funcionario random.

--- src/main/java/com/tuyo/funcionariosdata/entities/Funcionario.java
+++ src/main/java/com/tuyo/funcionariosdata/entities/Funcionario.java
@@ -1,13 +1,17 @@
 package com.tuyo.funcionariosdata.entities;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 
 @Entity
 public class Funcionario {
 
-    @TableGenerator(name = "funcionarios_gen",table = "ger_id",pkColumnName = "ger_name",valueColumnName = "ger_val ",allocationSize=100)        //Colocamos um nome para a tabela geradora. E em table, o nome da tabela como está no banco.
+   // @TableGenerator(name = "funcionarios_gen",table = "ger_id",pkColumnName = "ger_name",valueColumnName = "ger_val ",allocationSize=100)        //Colocamos um nome para a tabela geradora. E em table, o nome da tabela como está no banco.
+    @GenericGenerator(name = "func_id",strategy = "com.tuyo.funcionariosdata.GeradorIDRandom")
+    @GeneratedValue(generator = "func_id")
     @Id                                                                                                                                             // Informamos com TableGenerator qual a nossa tabela geradora e suas informacoes.
-    @GeneratedValue(strategy= GenerationType.TABLE,generator = "funcionarios_gen")                                                               // Ao utilizar TableGenerator se faz necesário colocar no strategy TABLE e generator.
+   // @GeneratedValue(strategy= GenerationType.TABLE,generator = "funcionarios_gen")                                                               // Ao utilizar TableGenerator se faz necesário colocar no strategy TABLE e generator.
     private int id;                                                                                                                             // O gerador de tabela TableGenerator prover a informacao da tabela, coluna, tamanho da alocação, e foi usado em GeneratedValue um strategy com a tabela chamada corretamente.
     private String name;
 
